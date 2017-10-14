4806e5ad68942649659a1b0ae5c0cd047002f47f
Tortera1999 <nikhilnarendra1999@gmail.com>
2017-10-14 18:42:14 -0400

    created the ui

--- WalmartBucket/WalmartBucket/ViewController.swift
+++ WalmartBucket/WalmartBucket/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ClickMeButton: UIButton!
+    @IBOutlet weak var WalmartLabel: UILabel!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    var count = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func ClickMeAction(_ sender: UIButton) {
+     let backgroundImage1 = UIImage(named: "Image1.jpg")
+    let backgroundImage2 = UIImage(named: "Image2.jpg")
+        
+        if(count % 2 == 0){
+            backgroundImage.image = backgroundImage1
+            count+=1
+        }
+        else{
+            backgroundImage.image = backgroundImage2
+            count += 1
+        }
+        
+        
+    }
 }
 
