3c3412cd09042d3dbaf4a1afe8b44f693e1515cf
akaisani <akaisani@purdue.edu>
2017-11-10 19:19:30 -0500

    fixed dual segue bug

---
+++ WalmartBucket/WalmartBucket/Bucketitem.swift
@@ -0,0 +1,21 @@
+//
+//  Bucketitem.swift
+//  WalmartBucket
+//
+//  Created by Abid Amirali on 11/10/17.
+//  Copyright © 2017 Nikhil Iyer. All rights reserved.
+//
+
+import Foundation
+
+class BucketItem {
+    let price: Double
+    let quantity: Int
+    
+    init(price: Double, quantity: Int) {
+        self.price = price
+        self.quantity = quantity
+    }
+    
+    
+}
