64dcbf2ad93d7bcd406a0d13c60fcd4d39daf1bc
akaisani <akaisani@purdue.edu>
2017-11-22 17:48:40 -0500

    created walmart item class

--- WalmartBucket/WalmartBucket/WalmartItem.swift
+++ WalmartBucket/WalmartBucket/WalmartItem.swift
@@ -6,16 +6,40 @@
 //  Copyright © 2017 Nikhil Iyer. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class WalmartItem{
-    let name;
-    let price;
-    let image;
-    let addToCartUrl;
-    let itemUrl;
+    let name: String;
+    let price: Double;
+    var imageURL: String {
+        get {
+            print("image url received")
+            return self.imageURL
+        }
+        set {
+            let url = URL(string: imageURL);
+            do {
+            let data1 = try Data(contentsOf: url!);
+            let image = UIImage(data: data1)
+            self.image = image!
+
+            } catch {
+                print("error when retriving image, check url")
+                self.image = nil
+            }
+            
+        }
+    }
+    let addToCartUrl: URL?;
+    let itemUrl: URL?;
+    var image: UIImage?;
     
-    init(<#parameters#>) {
-        <#statements#>
+    
+    
+    init(name: String, price: Double, imageURL: String, addToCartURL: String, itemURL: String) {
+        self.name = name
+        self.price = price
+        self.addToCartUrl = URL(string: addToCartURL)
+        self.itemUrl = URL(string: itemURL)
     }
 }
