03f49aed5dffdfee1014c6c8cc814006c247e695
Tortera1999 <nikhilnarendra1999@gmail.com>
2017-11-22 17:21:34 -0500

    added new walmart class

---
+++ WalmartBucket/WalmartBucket/WalmartItem.swift
@@ -0,0 +1,21 @@
+//
+//  WalmartItem.swift
+//  WalmartBucket
+//
+//  Created by Nikhil Iyer on 11/22/17.
+//  Copyright © 2017 Nikhil Iyer. All rights reserved.
+//
+
+import Foundation
+
+class WalmartItem{
+    let name;
+    let price;
+    let image;
+    let addToCartUrl;
+    let itemUrl;
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+}
